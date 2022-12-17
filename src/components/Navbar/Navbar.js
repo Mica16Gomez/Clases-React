cb04cfa0e5c48310cb0bc63b8707f65ba22a182a
micag <mica16gomez@gmail.com>
2022-12-17 16:14:49 -0300

    Clase 1-2-3

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+		<nav className='navbar'>
+			<h1 className='title'>BROKER</h1>
+			<button className='boton1'>INICIO</button>
+            <button className='boton1'>PRODUCTOS</button>
+            <button className='boton1'>CONTACTO</button>
+        </nav>
+    )
+}
+
+export default Navbar
