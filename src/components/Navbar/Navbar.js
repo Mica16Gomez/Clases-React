300e93fbe4e71388040cfeef3fa90fee5db609bf
micag <mica16gomez@gmail.com>
2022-12-21 15:33:57 -0300

    clase de componentes2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,31 @@
 import './Navbar.css'
+import Button  from '../Button/Button'
+import CartWidget from '../CartWidget/CartWidget'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    console.log(props)
+
+    const handlOnClick = () => {
+        alert('hice click')
+    }
+
+    const handlOnClick2 = () => {
+        alert('hice otro click')
+    }
+
+
     return (
 		<nav className='navbar'>
-			<h1 className='title'>BROKER</h1>
-			<button className='boton1'>INICIO</button>
-            <button className='boton1'>PRODUCTOS</button>
-            <button className='boton1'>CONTACTO</button>
+			<h1 style={{ color: props.colorTitle}} >BROKER 
+            <img src='./images/skateboarder.png'/> </h1> 
+			<Button colorText='black' func={handlOnClick}>INICIO</Button>
+            <Button colorText='black' func={handlOnClick2}>PRODUCTOS</Button>
+            <Button>CONTACTO</Button>
+            <Button>LOGIN</Button>
+            <CartWidget />
+  
+           
+            
         </nav>
     )
 }
