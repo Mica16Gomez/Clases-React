6df789374d7b48b24f2d1e95d416f943e0197920
micag <mica16gomez@gmail.com>
2022-12-26 23:14:29 -0300

    calse 7 apis

---
+++ src/MercadoLibre/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MercadoLibre from './MercadoLibre/MercadoLibre';
+
+function App() {
+  return (
+    <div className="App">
+      <MercadoLibre />
+    </div>
+  );
+}
+
+export default App;
